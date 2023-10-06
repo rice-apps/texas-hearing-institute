8f5bc6364f98f71e2b184e73aabb49724360c5f3
Shivam Pathak <shivampathak108@outlook.com>
2023-10-05 20:22:33 -0500

    added tailwind

--- texas-hearing-institute/App.tsx
+++ texas-hearing-institute/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={tw`text-3xl font-bold underline`}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
