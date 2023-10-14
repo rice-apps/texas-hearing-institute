17d58ce8c6164b1f57c6a9a7648692bbbf84587e
Vincent Wang <51209452+vincentxwang@users.noreply.github.com>
2023-10-14 15:32:09 -0500

    Created TestComponent

--- texas-hearing-institute/App.tsx
+++ texas-hearing-institute/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
+import React, {useState, useEffect, Component} from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={tw`text-3xl font-bold underline`}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TestComponent text="random color changer"></TestComponent>
     </View>
   );
 }
@@ -14,8 +15,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    height: 10,
+    backgroundColor: '#1f1e33',
   },
 });
+
+const TestComponent = ({ text } : { text : any }) => {
+  const [color1, setColor1] = useState(randomRgb());
+  const [color2, setColor2] = useState(randomRgb());
+  const [color3, setColor3] = useState(randomRgb());
+
+  const handleClick = () => {
+    setColor1(randomRgb());
+    setColor2(randomRgb());
+    setColor3(randomRgb());
+  }
+
+  return(
+    <View style={[
+      styles.container,
+      {
+        flexDirection: 'column'
+      }
+    ]}>
+      <Text style={{flex: 1, textAlign: 'center', marginTop: 10, fontSize: 30, color: 'white'}}>{text}</Text>
+      <View style={{backgroundColor: color1, flex: 1}}></View>
+      <View style={{backgroundColor: color2, flex: 2}}></View>
+      <View style={{backgroundColor: color3, flex: 3}}></View>
+      <Button title="change color" onPress={
+        handleClick
+      }></Button>
+    </View>
+  )
+}
+
+const randomRgb = () => {
+  return `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`
+};
+
