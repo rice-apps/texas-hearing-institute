d2d75065e0d773a5082ce29428ae24d57fcb40e2
Chloe Lim <chloelim608@gmail.com>
2023-10-14 15:08:41 -0500

    play

--- texas-hearing-institute/App.tsx
+++ texas-hearing-institute/App.tsx
@@ -1,15 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={tw`text-3xl font-bold underline`}>Open up App.tsx to start working on your app!</Text>
+      <Text style={tw`text-3xl font-extralight italic underline`}>Open up App.tsx to start working on your app!</Text>
+      <Button
+        // style={styles.loginScreenButton}
+        title="Press me"
+        color="#FFFFFF"
+        onPress={() => Alert.alert('First Button pressed')}
+      />
+      {/* <TouchableOpacity
       
-      <Text>Hello world.</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text>Press Here</Text>
+        onPress={() => Alert.alert('Simple Button pressed')}
+      /> */}
+      <TouchableOpacity style={styles.loginScreenButton} 
+      onPress={() => Alert.alert('Second Button pressed')}>
+        <Text style={styles.text}>Press Here</Text>
+      </TouchableOpacity>
+        
+    </View >
   );
 }
 
@@ -21,4 +34,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  loginScreenButton: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#000000',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  text:{
+    color: '#ffff'
+  }
 });
