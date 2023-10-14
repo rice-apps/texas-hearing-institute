a535d62f11ac087611dd7adf31231b59f19c67f4
Chloe Lim <chloelim608@gmail.com>
2023-10-14 14:48:01 -0500

    trial1

--- texas-hearing-institute/App.tsx
+++ texas-hearing-institute/App.tsx
@@ -6,6 +6,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={tw`text-3xl font-bold underline`}>Open up App.tsx to start working on your app!</Text>
+      
+      <Text>Hello world.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 24,
+    backgroundColor: '#f01',
     alignItems: 'center',
     justifyContent: 'center',
   },
