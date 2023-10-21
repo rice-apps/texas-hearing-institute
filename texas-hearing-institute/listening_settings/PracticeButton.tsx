c3b9b6afd556110e3a34e2c057b6a635c9a69ca5
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2023-10-21 15:36:52 -0500

    Add listening settings page

---
+++ texas-hearing-institute/listening_settings/PracticeButton.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { Pressable, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+export default function PracticeButton() {
+
+    return (
+        <View>
+            <TouchableOpacity
+                style={[styles.button]}
+            >   
+                <View style={styles.container}>
+                    <Text style={styles.buttonText}>Let's Practice</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    },
+    button: {
+        backgroundColor: "#D9D9D9",
+        borderRadius: 20,
+        height: 54,
+        marginTop: 6,
+        marginBottom: 6
+    },
+    buttonText: {
+        fontSize: 20
+    }
+})
