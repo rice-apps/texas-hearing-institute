cc1710a6966c7b546deca32c2cc4a040ec2652e7
Shivam Pathak <shivampathak108@outlook.com>
2023-10-21 15:31:00 -0500

    supabase setup

---
+++ texas-hearing-institute/lib/supabase.ts
@@ -0,0 +1,14 @@
+import 'react-native-url-polyfill/auto'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+import { SUPABASE_URL, SUPABASE_KEY } from "@env"
+
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
