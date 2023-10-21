51b0f98641b3d8661e0403829261cd434e12ff13
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2023-10-21 15:36:52 -0500

    Add listening settings page

--- texas-hearing-institute/App.tsx
+++ texas-hearing-institute/App.tsx
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import tw from 'tailwind-react-native-classnames';
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import ListeningSettings from './listening_settings/ListeningSettings';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={tw`text-3xl font-bold underline`}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex: 1}} >
+      <ListeningSettings />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
