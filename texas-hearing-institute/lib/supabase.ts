71d13f47c14f72b78677762667435900a62e3a86
Baylee Keevan <jade@Baylees-Laptop.local>
2023-10-28 16:02:48 -0500

    setup eslint + prettier + pre-commit hook

--- texas-hearing-institute/lib/supabase.ts
+++ texas-hearing-institute/lib/supabase.ts
@@ -1,14 +1,13 @@
-import 'react-native-url-polyfill/auto'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient } from '@supabase/supabase-js'
-import { SUPABASE_URL, SUPABASE_KEY } from "@env"
-
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_KEY } from '@env';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-})
+	auth: {
+		storage: AsyncStorage,
+		autoRefreshToken: true,
+		persistSession: true,
+		detectSessionInUrl: false,
+	},
+});
