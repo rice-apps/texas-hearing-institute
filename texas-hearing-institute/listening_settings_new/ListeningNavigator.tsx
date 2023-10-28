7b724b02aba7a23d589e376aec5a368296d4bb1d
DerickPascual <derickvanpascual@gmail.com>
2023-10-28 16:51:04 -0500

    Reorganized files, fixed a bug where ListeningNavigator wasn't displaying correctly

--- texas-hearing-institute/listening_settings_new/ListeningNavigator.tsx
+++ texas-hearing-institute/listening_settings_new/ListeningNavigator.tsx
@@ -1,20 +1,25 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import PracticeTab from './PracticeTab';
-import PlaceCue from './PlaceCue';
-import {SafeAreaView} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PracticeTab from './tabs/PracticeTab';
+import PlaceCueTab from './tabs/PlaceCueTab';
+import { ApplicationProvider } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
 
 const Stack = createNativeStackNavigator();
 
+// This is the navigator of listening settings
+// Use this componenet in App.tsx as follows:
+// return <ListeningNavigator /> to see listening navigator
 export default function ListeningNavigator() {
     return (
-        <NavigationContainer>
-            <SafeAreaView style={{flex: 1}}>
-                <Stack.Navigator>
-                    <Stack.Screen name="Practice" component={PracticeTab}/>
-                    <Stack.Screen name="Place Cue" component={PlaceCue}/>
-                </Stack.Navigator>
-            </SafeAreaView>
-        </NavigationContainer>
+        <ApplicationProvider {...eva} theme={eva.light}>
+            <NavigationContainer>
+                    <Stack.Navigator initialRouteName='Practice'>
+                        <Stack.Screen name="Practice" component={PracticeTab} />
+                        <Stack.Screen name="Place Cue" component={PlaceCueTab} />
+                        {/* Other screens not implemented yet */}
+                    </Stack.Navigator>
+            </NavigationContainer>
+        </ApplicationProvider>
     )
 }
