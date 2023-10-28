fe6f844c398a9e20def02ef0bef94d24033f8039
pallob101 <pallobpaul366@gmail.com>
2023-10-28 13:32:18 -0500

    File Structure Changed

--- texas-hearing-institute/App.tsx
+++ texas-hearing-institute/App.tsx
@@ -1,22 +1,7 @@
 
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import SpeechSetting from './pages/SpeechSetting';
-import Comelater from './pages/ComeLater';
-
-const Stack = createStackNavigator();
-
+import SpeechSetup from "./pages/Home";
 function App() {
-  return (
-    <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Speech Setting Home Screen" component={SpeechSetting} />
-      <Stack.Screen name="Coming Soon" component={Comelater} />
-
-    </Stack.Navigator>
-  </NavigationContainer>
-  );
+  return <SpeechSetup/>
 }
 
 export default App;
--- texas-hearing-institute/pages/ComeLater.tsx
+++ texas-hearing-institute/pages/ComeLater.tsx
@@ -1,9 +1,20 @@
+import { ScrollView } from 'react-native';
 import Heading from '../components/Heading';
+import Subheading from '../components/SubHeading';
 
 
 const Comelater: React.FC = () => {
+  const handleSubmission = (buttonStates: boolean[]) => {
+    console.log('Button states:', buttonStates);
+  };
+
+  const buttonLabels = ['Button 1', 'Button 2', 'Button 3'];
   return (
-    <Heading title={"Coming Soon."}></Heading>
+    <ScrollView>
+    <Heading title={"Initial Consonants"}></Heading>
+    <Subheading title={"Select a consonant and blahblah to blahblah practise speaking"}></Subheading>
+    </ScrollView>
+   
   )
 }
 
