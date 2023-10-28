60d9c51a84546c5f0ccb8375637dc104ed1acc37
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2023-10-28 14:33:13 -0500

    Created PlaceCue page

--- texas-hearing-institute/App.tsx
+++ texas-hearing-institute/App.tsx
@@ -1,10 +1,14 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
-import ListeningSettings from './listening_settings/ListeningSettings';
+import {SafeAreaView} from 'react-native';
+import ListeningSettings from './listening_settings_new/ListeningSettings';
+import {ApplicationProvider} from "@ui-kitten/components";
+import * as eva from '@eva-design/eva';
 
 export default function App() {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <ListeningSettings />
-    </SafeAreaView>
-  );
+    return (
+        <ApplicationProvider {...eva} theme={eva.light}>
+            <SafeAreaView style={{flex: 1}}>
+                <ListeningSettings/>
+            </SafeAreaView>
+        </ApplicationProvider>
+    );
 }
