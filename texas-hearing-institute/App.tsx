b56a6f15e41cb73df92d4c1cee0f85f94755f5ea
DerickPascual <derickvanpascual@gmail.com>
2023-10-28 14:40:15 -0500

    Progress on buttongroup component

--- texas-hearing-institute/App.tsx
+++ texas-hearing-institute/App.tsx
@@ -1,10 +1,10 @@
 import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
-import ListeningSettings from './listening_settings/ListeningSettings';
+import PracticeTab from './listening_settings_new/PracticeTab';
 
 export default function App() {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <ListeningSettings />
+      <PracticeTab />
     </SafeAreaView>
   );
 }
