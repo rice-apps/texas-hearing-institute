829ae7102a321c8e70e1e2e913dc5fb2d57cbcad
DerickPascual <derickvanpascual@gmail.com>
2023-10-28 14:40:15 -0500

    Progress on buttongroup component

--- texas-hearing-institute/listening_settings_new/PracticeTab.tsx
+++ texas-hearing-institute/listening_settings_new/PracticeTab.tsx
@@ -1,9 +1,33 @@
 import { View, StyleSheet } from "react-native";
+import ButtonGroup from './ButtonGroup';
 
-export default function ListeningSettings() {
+const headerImage = require('./images/active-listening.png');
+
+const headerText = "Listening Babble"
+
+const buttonLabels = [
+    "Variegated Vowels",
+    "Manner",
+    "Place Cue",
+    "Voicing"
+]
+
+const buttonRoutes = [
+    "variegated-vowels",
+    "manner",
+    "place-cue",
+    "voicing"
+]
+
+export default function PracticeTab() {
     return (
         <View style={[styles.margins, styles.expanded]}>
-            
+            <ButtonGroup 
+                headerImage = {headerImage}
+                headerText = {headerText}
+                buttonLabels = {buttonLabels}
+                buttonRoutes = {buttonRoutes}
+            />
         </View>
     );
 }
