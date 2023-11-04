86068d471023e489e2d771a4a1951ca98a7336a5
DerickPascual <derickvanpascual@gmail.com>
2023-11-04 13:54:05 -0500

    Added blank screens for manner, voicing, var vowels

---
+++ texas-hearing-institute/listening_settings_new/tabs/MannerScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function MannerScreen() {
+    return (
+        <View>
+            <Text>Manner</Text>
+        </View>
+    )
+}
---
+++ texas-hearing-institute/listening_settings_new/tabs/VoicingScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function VoicingScreen() {
+    return (
+        <View>
+            <Text>Voicing</Text>
+        </View>
+    )
+}
