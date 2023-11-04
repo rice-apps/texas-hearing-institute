1f06ad59766a5273e05370ffeb11a44dbbda63a3
DerickPascual <derickvanpascual@gmail.com>
2023-11-04 14:38:14 -0500

    Created/Implemented ScreenView, SubTitle, and Title components

--- texas-hearing-institute/listening_settings_new/tabs/VoicingScreen.tsx
+++ texas-hearing-institute/listening_settings_new/tabs/VoicingScreen.tsx
@@ -1,9 +1,14 @@
 import { View, Text } from 'react-native';
+import TitleText from '../components/TitleText';
+import SubTitleText from '../components/SubTitleText';
+import ScreenView from '../components/ScreenView';
 
 export default function VoicingScreen() {
     return (
-        <View>
-            <Text>Voicing</Text>
-        </View>
+        <ScreenView>
+            <TitleText>Voicing</TitleText>
+            <SubTitleText>Do something with voicing</SubTitleText>
+            {/*Implement rest of voicing here*/}
+        </ScreenView>
     )
 }
