ec0173bf83e443a5721c4a4817085f7addc90f19
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2023-11-04 13:24:21 -0500

    Start implementing grid

---
+++ texas-hearing-institute/listening_settings_new/components/ToggleGridButtonsComponent/ToggleButtonStyle.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    button: {
+        paddingHorizontal: 16,
+        paddingTop: 8,
+        paddingBottom: 8,
+        borderRadius: 10, 
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+    },
+    buttonActive: {
+        backgroundColor: 'blue',
+        borderColor: 'white',
+    },
+    buttonInactive: {
+        backgroundColor: '#F5F5F5',
+        borderColor: '#D9D9D9',
+        color: 'black',
+    },
+    buttonText: {
+        fontSize: 20, 
+    },
+    buttonTextActive: {
+        color: 'white',
+    },
+    buttonTextInactive: {
+        color: 'black',
+    },
+});
