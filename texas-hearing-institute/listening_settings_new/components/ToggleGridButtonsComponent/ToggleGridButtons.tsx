50d8f83c49a8624516af7d3510d1e260a324e5fc
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2023-11-04 13:24:21 -0500

    Start implementing grid

---
+++ texas-hearing-institute/listening_settings_new/components/ToggleGridButtonsComponent/ToggleGridButtons.tsx
@@ -0,0 +1,42 @@
+import { View, StyleSheet } from 'react-native';
+import ToggleButton from './ToggleButton';
+
+interface Props {
+    items: string[];
+    itemsSelected: boolean[];
+    setItemsSelected: (index: number, newValue: boolean) => void;
+    disabled?: boolean
+}
+
+const ToggleGridButtons: React.FC<Props> = ({ items, itemsSelected, setItemsSelected,disabled }) => {
+    return (
+        <View style={styles.container}>
+            {items.map((item, index) => (
+                <View style={{
+                    marginRight: 12,
+                    marginBottom: 12,
+                }} key={index}>
+                    <ToggleButton 
+                        title={item}
+                        isToggled={itemsSelected[index]} 
+                        onToggle={
+                            (newValue: boolean) => {
+                            disabled ? () => {} : setItemsSelected(index, newValue) 
+                        }}
+                    />
+                </View>
+            ))}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+        width: '100%',
+    },
+});
+    
+export default ToggleGridButtons;
