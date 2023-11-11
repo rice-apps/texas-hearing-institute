145c9d0f03af577a34b43f34770fe8db62c71635
Baylee Keevan <jade@baylees-laptop.dyndns.rice.edu>
2023-11-11 14:27:36 -0600

    format

--- texas-hearing-institute/App.tsx
+++ texas-hearing-institute/App.tsx
@@ -1,6 +1,24 @@
-import SpeechNavigation from './pages/SpeechSetup/SpeechNavigation';
-function App() {
-	return <SpeechNavigation />;
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<Text style={tw`text-3xl font-bold underline`}>
+				Open up App.tsx to start working on your app!
+			</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
-export default App;
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
--- texas-hearing-institute/components/GridButton.tsx
+++ texas-hearing-institute/components/GridButton.tsx
@@ -1,95 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import { ScrollView,  TouchableOpacity, Text, View, StyleSheet  } from 'react-native';
-import ToggleableButton from './ToggleableButton';
+import React, { useState } from 'react';
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
 
 const GridButton: React.FC = () => {
-  
-  return (
-    <SingleSelectionButtons/>
-    // <View>
-    //   <View style={buttonStyles.container}>{renderButtons()}</View>
-    //   {/* <Text style={{ fontSize: 18, fontWeight: 'bold', margin: 10 }}>
-    //     Toggled Buttons: {Object.keys(buttonData).filter((label) => buttonData[label]).join(', ')}
-    //   </Text> */}
-    // </View>
-  );
+	return <SingleSelectionButtons />;
 };
 
 const buttonStyles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignContent:"center",
-    padding: 15
-  },
-}
-);
+	container: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		alignContent: 'center',
+		padding: 15,
+	},
+});
 
-export default GridButton
-
+export default GridButton;
 
 const SingleSelectionButtons = () => {
-  const [selectedButton, setSelectedButton] = useState(null);
+	const [selectedButton, setSelectedButton] = useState(null);
 
-  const handleButtonPress = (buttonId: any) => {
-    setSelectedButton(buttonId);
-  };
+	const handleButtonPress = (buttonId: any) => {
+		setSelectedButton(buttonId);
+	};
 
-  const renderButtons = () => {
-    const buttonData = [
-      { id: 1, label: 'A' },
-      { id: 2, label: 'B' },
-      { id: 3, label: 'ah'},
-      { id: 4, label: 'bu'},
-      { id: 5, label: 'me'},
-      { id: 6, label: 'yo'},
-      { id: 7, label: 'io'},
-      { id: 8, label: 'po'},
-      { id: 9, label: 'to'},
-      { id: 10, label: 'ru'},
-      { id: 11, label: "do"},
-      { id: 12, label: 'r'},
-      { id: 13, label: "d"},
-      { id: 14, label: 'r'},
-      { id: 15, label: "d"},
+	const renderButtons = () => {
+		const buttonData = [
+			{ id: 1, label: 'A' },
+			{ id: 2, label: 'B' },
+			{ id: 3, label: 'ah' },
+			{ id: 4, label: 'bu' },
+			{ id: 5, label: 'me' },
+			{ id: 6, label: 'yo' },
+			{ id: 7, label: 'io' },
+			{ id: 8, label: 'po' },
+			{ id: 9, label: 'to' },
+			{ id: 10, label: 'ru' },
+			{ id: 11, label: 'do' },
+			{ id: 12, label: 'r' },
+			{ id: 13, label: 'd' },
+			{ id: 14, label: 'r' },
+			{ id: 15, label: 'd' },
 
-      // Add more buttons as needed
-    ];
+			// Add more buttons as needed
+		];
 
-    return buttonData.map((button) => (
-      <TouchableOpacity
-        key={button.id}
-        style={[styles.button, selectedButton === button.id && styles.selectedButton]}
-        onPress={() => handleButtonPress(button.id)}
-      >
-        <Text style={styles.buttonText}>{button.label}</Text>
-      </TouchableOpacity>
-    ));
-  };
+		return buttonData.map((button) => (
+			<TouchableOpacity
+				key={button.id}
+				style={[
+					styles.button,
+					selectedButton === button.id && styles.selectedButton,
+				]}
+				onPress={() => handleButtonPress(button.id)}
+			>
+				<Text style={styles.buttonText}>{button.label}</Text>
+			</TouchableOpacity>
+		));
+	};
 
-  return <View style={styles.container}>{renderButtons()}</View>;
+	return <View style={styles.container}>{renderButtons()}</View>;
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    margin: 10,
-    flexWrap: "wrap",
-
-  },
-  button: {
-    padding: 10,
-    backgroundColor: 'white',
-    borderRadius: 5,
-    margin: 5
-  },
-  selectedButton: {
-    backgroundColor: 'grey',
-  },
-  buttonText: {
-    color: 'black',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+	container: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		margin: 10,
+		flexWrap: 'wrap',
+	},
+	button: {
+		padding: 10,
+		backgroundColor: 'white',
+		borderRadius: 5,
+		margin: 5,
+	},
+	selectedButton: {
+		backgroundColor: 'grey',
+	},
+	buttonText: {
+		color: 'black',
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
 });
