0f525d132d6afa7d29869ed0c73ad825c2d9798e
Michel <legitmichel777@outlook.com>
2023-11-11 14:58:29 -0600

    Removed node_modules

--- texas-hearing-institute/utilComponents/ToggleGridButtonsComponent/ToggleButton.tsx
+++ texas-hearing-institute/utilComponents/ToggleGridButtonsComponent/ToggleButton.tsx
@@ -1,5 +1,6 @@
 import { Text, TouchableOpacity } from 'react-native';
 import styles from './ToggleButtonStyle';
+import * as Speech from 'expo-speech'
 
 interface Props {
     title: string;
@@ -9,8 +10,17 @@ interface Props {
 }
 
 const ToggleButton: React.FC<Props> = ({ title, isToggled, onToggle, enabled }) => {
+
+    const speak = (text:string) : void => {
+        Speech.speak(text,{rate:1,pitch:1,volume:1})
+    };
+
     return (
-        <TouchableOpacity style={[styles.button, isToggled ? styles.buttonActive : styles.buttonInactive]} onPress={() => onToggle(!isToggled)} disabled={!enabled}>
+        <TouchableOpacity style={[styles.button, isToggled ? styles.buttonActive : styles.buttonInactive]} onPress={() => {
+                onToggle(!isToggled);
+                speak(title)
+            }
+            } disabled={!enabled}>
             <Text style={[styles.buttonText, isToggled ? styles.buttonTextActive : styles.buttonTextInactive]}>{title}</Text>
         </TouchableOpacity>
     );
