cb5537e85cabdf2af0fa0d33c5e8f3146cb13037
Vincent Wang <51209452+vincentxwang@users.noreply.github.com>
2023-11-18 14:06:22 -0600

    changed radio component to match

--- texas-hearing-institute/pages/SpeechSetup/InitialConsonants.tsx
+++ texas-hearing-institute/pages/SpeechSetup/InitialConsonants.tsx
@@ -1,4 +1,5 @@
 import { ScrollView, View } from 'react-native';
+import { useState } from 'react';
 import Heading from '../../components/Heading';
 import Subheading from '../../components/SubHeading';
 import GridButton from '../../components/GridButton';
@@ -6,6 +7,9 @@ import Selection from '../../components/Selection';
 import { Alert } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import PracticeButton from '../../components/PracticeButton';
+import RadioButtonGrid from '../../components/RadioButtonGrid/RadioButtonGrid';
+// import RadioButtonGrid from '../../components/RadioButtonGrid/RadioButtonGrid';
+
 
 const InitialConsonants = () => {
 	const showAlert = () => {
@@ -18,6 +22,25 @@ const InitialConsonants = () => {
 		]);
 	};
 
+	const modeOptions = [
+		'Voicing',
+		'Manner',
+		'Place Cue',
+	]
+	const [selectedModeOptionsIndex, setselectedModeOptionsIndex] = useState(0)
+
+	const vowelOptions = [
+		'Same Vowels',
+		'Different Vowels',
+	]
+	const [selectedVowelOptionsIndex, setselectedVowelOptionsIndex] = useState(0)
+
+	const speedOptions = [
+		'Slow: 4 Syllables',
+		'Fast: 12 Syllabes'
+	]
+	const [selectedSpeedOptionsIndex, setselectedSpeedOptionsIndex] = useState(0)
+
 	return (
 		<ScrollView>
 			<Heading title={'Initial Consonants'}></Heading>
@@ -25,9 +48,33 @@ const InitialConsonants = () => {
 				title={'Select a consonant'}
 			></Subheading>
 			<GridButton />
-			<Selection
-				buttonNames={['Variegated Vowels', 'Voicing', 'Manner', 'Place Cue']}
-			></Selection>
+			<View style={tw`p-4 flex-col`}>
+				<RadioButtonGrid
+				items={modeOptions}
+				label={'Select mode'}
+				onSelect={newValue => {
+					setselectedModeOptionsIndex(newValue)
+				}}
+				selectedItemIndex={selectedModeOptionsIndex}
+				/>
+				<RadioButtonGrid
+				items={vowelOptions}
+				label={'Select vowel type'}
+				onSelect={newValue => {
+					setselectedVowelOptionsIndex(newValue)
+				}}
+				selectedItemIndex={selectedVowelOptionsIndex}
+				/>
+				<RadioButtonGrid
+				items={speedOptions}
+				label={'Select speed'}
+				onSelect={newValue => {
+					setselectedSpeedOptionsIndex(newValue)
+				}}
+				selectedItemIndex={selectedSpeedOptionsIndex}
+				/>
+			</View>
+
 			<View style={tw`p-4`}></View>
 			<PracticeButton label="Let's Practice" onPress={showAlert} />
 		</ScrollView>
