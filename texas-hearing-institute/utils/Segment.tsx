6f3048f377efd64a538696592d3565489a8dbff6
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2024-01-27 13:57:24 -0600

    Create Segment

---
+++ texas-hearing-institute/utils/Segment.tsx
@@ -0,0 +1,19 @@
+class Segment {
+	name: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class ConsonantSegment extends Segment {
+	categories: ConsonantCategories[];
+	mannerPetalGroups: number[];
+	placePetalGroups: number[];
+	voicePetalGroups: number[];
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class VowelSegment extends Segment {}
+
+enum ConsonantCategories {
+	Initial,
+	Final,
+}
