40e303cdc375f27bc5f3a5428c46527f1328dc9c
DerickPascual <derickvanpascual@gmail.com>
2024-01-27 15:30:59 -0600

    Implemented basic login functionality

---
+++ clinician-dashboard/app/auth/logout/route.tsx
@@ -0,0 +1,22 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { type NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const supabase = createRouteHandlerClient({ cookies });
+
+  // Check if we have a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  console.log(session);
+
+  if (session) {
+    await supabase.auth.signOut();
+  }
+
+  return NextResponse.redirect(new URL("/", req.url), {
+    status: 302,
+  });
+}
