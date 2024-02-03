57e2665da22d2630c503e771e3c1f58b97f94cdc
DerickPascual <derickvanpascual@gmail.com>
2024-02-03 14:39:00 -0600

    Added redirect for logged in users if they go to /auth/login

--- clinician-dashboard/middleware.tsx
+++ clinician-dashboard/middleware.tsx
@@ -54,7 +54,13 @@ export async function middleware(request: NextRequest) {
     }
   )
 
-  await supabase.auth.getUser()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user && request.nextUrl.pathname === '/auth/login') {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
 
   return response
 }
