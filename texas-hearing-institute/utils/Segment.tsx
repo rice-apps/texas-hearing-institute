049c5578fddcba6cc41fe3ac2b3ef6f81d38f4fa
Vincent Wang <51209452+vincentxwang@users.noreply.github.com>
2024-02-03 15:38:19 -0600

    added final consonants to syllable generation

--- texas-hearing-institute/utils/Segment.tsx
+++ texas-hearing-institute/utils/Segment.tsx
@@ -1,92 +1,92 @@
-import {AllSegments} from "./AllSegmentsHardcoded";
+import { AllSegments } from './AllSegmentsHardcoded';
 
 export class Segment {
-    name = '';
+	name = '';
 
-    constructor(name: string) {
-        this.name = name;
-    }
+	constructor(name: string) {
+		this.name = name;
+	}
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export class ConsonantSegment extends Segment {
-    // Categories can contain either ConsonantCategories.initial, .final, or both
-    categories: ConsonantCategories[] = [];
+	// Categories can contain either ConsonantCategories.initial, .final, or both
+	categories: ConsonantCategories[] = [];
 
-    // Maps "ConsonantFlower.XXX" to the petal IDs that this consonant belongs to in that flower
-    flowerToPetalIds = new Map<ConsonantFlower, number[]>([
-        [ConsonantFlower.Manner, []],
-        [ConsonantFlower.Voice, []],
-        [ConsonantFlower.Place, []],
-    ]);
+	// Maps "ConsonantFlower.XXX" to the petal IDs that this consonant belongs to in that flower
+	flowerToPetalIds = new Map<ConsonantFlower, number[]>([
+		[ConsonantFlower.Manner, []],
+		[ConsonantFlower.Voice, []],
+		[ConsonantFlower.Place, []],
+	]);
 
-    getPetalIds(consonantFlower: ConsonantFlower): number[] {
-        // Return the corresponding petal IDs (or, for null safety, an empty array if the value doesn't exist)
-        return this.flowerToPetalIds.get(consonantFlower) ?? [];
-    }
+	getPetalIds(consonantFlower: ConsonantFlower): number[] {
+		// Return the corresponding petal IDs (or, for null safety, an empty array if the value doesn't exist)
+		return this.flowerToPetalIds.get(consonantFlower) ?? [];
+	}
 
-    // Fetch other ConsonantSegments from canSayInventory that share any of the petalIds in this.getPetalIds
-    fetchConsonantSiblings(flower: ConsonantFlower): ConsonantSegment[] {
-        const petalIds = this.getPetalIds(flower);
+	// Fetch other ConsonantSegments from canSayInventory that share any of the petalIds in this.getPetalIds
+	fetchConsonantSiblings(flower: ConsonantFlower): ConsonantSegment[] {
+		const petalIds = this.getPetalIds(flower);
 
-        // TODO: Assuming canSayInventory is an array of Segments
-        //  We would want to read this from async storage
-        const canSayInventory: Segment[] = AllSegments.getAllSegmentsHardcoded();
+		// TODO: Assuming canSayInventory is an array of Segments
+		//  We would want to read this from async storage
+		const canSayInventory: Segment[] = AllSegments.getAllSegmentsHardcoded();
 
-        console.log("4", canSayInventory)
-        
-        // Use filter to ensure x is of type ConsonantSegment
-        const consonantSegments: ConsonantSegment[] = canSayInventory.filter(
-            (x: Segment): x is ConsonantSegment => x instanceof ConsonantSegment,
-        );
+		console.log('4', canSayInventory);
 
-        console.log("5:", consonantSegments)
+		// Use filter to ensure x is of type ConsonantSegment
+		const consonantSegments: ConsonantSegment[] = canSayInventory.filter(
+			(x: Segment): x is ConsonantSegment => x instanceof ConsonantSegment,
+		);
 
-        // Use filter and intersection logic to check if there are common petalIds
-        const consonantSiblings = consonantSegments.filter(
-            (x: ConsonantSegment) =>
-                x.name != this.name && // Don't include our own ConsonantSegment as a sibling
-                x
-                    .getPetalIds(flower)
-                    .some((petalId: number) => petalIds.includes(petalId)),
-        );
-        console.log("6:", consonantSiblings)
-        return consonantSiblings;
-    }
+		console.log('5:', consonantSegments);
 
-    constructor(
-        name: string,
-        categories: ConsonantCategories[],
-        petalIds: {
-            manner?: number[];
-            voice?: number[];
-            place?: number[];
-        } = {},
-    ) {
-        super(name);
-        this.categories = categories;
-        this.flowerToPetalIds = new Map<ConsonantFlower, number[]>([
-            [ConsonantFlower.Manner, petalIds.manner ?? []],
-            [ConsonantFlower.Voice, petalIds.voice ?? []],
-            [ConsonantFlower.Place, petalIds.place ?? []],
-        ]);
-    }
+		// Use filter and intersection logic to check if there are common petalIds
+		const consonantSiblings = consonantSegments.filter(
+			(x: ConsonantSegment) =>
+				x.name != this.name && // Don't include our own ConsonantSegment as a sibling
+				x
+					.getPetalIds(flower)
+					.some((petalId: number) => petalIds.includes(petalId)),
+		);
+		console.log('6:', consonantSiblings);
+		return consonantSiblings;
+	}
+
+	constructor(
+		name: string,
+		categories: ConsonantCategories[],
+		petalIds: {
+			manner?: number[];
+			voice?: number[];
+			place?: number[];
+		} = {},
+	) {
+		super(name);
+		this.categories = categories;
+		this.flowerToPetalIds = new Map<ConsonantFlower, number[]>([
+			[ConsonantFlower.Manner, petalIds.manner ?? []],
+			[ConsonantFlower.Voice, petalIds.voice ?? []],
+			[ConsonantFlower.Place, petalIds.place ?? []],
+		]);
+	}
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export class VowelSegment extends Segment {
-    constructor(name: string) {
-        super(name);
-    }
+	constructor(name: string) {
+		super(name);
+	}
 }
 
 export enum ConsonantCategories {
-    Initial,
-    Final,
+	Initial,
+	Final,
 }
 
 export enum ConsonantFlower {
-    Manner,
-    Voice,
-    Place,
+	Manner,
+	Voice,
+	Place,
 }
--- texas-hearing-institute/utils/syllableGeneration.tsx
+++ texas-hearing-institute/utils/syllableGeneration.tsx
@@ -1,66 +1,101 @@
-import { Segment, ConsonantFlower, ConsonantCategories, VowelSegment, ConsonantSegment } from "./Segment";
-import { AllSegments } from "./AllSegmentsHardcoded";
+import {
+	Segment,
+	ConsonantFlower,
+	ConsonantCategories,
+	VowelSegment,
+	ConsonantSegment,
+} from './Segment';
+import { AllSegments } from './AllSegmentsHardcoded';
 
 // syllableGeneration returns an array of 2-4 words (i.e. [pee, paw]).
 // Inputs:
 // segment: is null during listening practice.
 // practiceTarget: is null to represent vowel targetting
 export function syllableGeneration(
-    segment: Segment | null,
-    consonantFlower: ConsonantFlower, 
-    isVariegatedVowel: boolean,
-    practiceTarget: ConsonantCategories | null,
-    numberOfWords: number,
+	segment: Segment | null,
+	consonantFlower: ConsonantFlower,
+	isVariegatedVowel: boolean,
+	practiceTarget: ConsonantCategories | null,
+	numberOfWords: number,
 ): string[] {
-    let petalConsonants: Segment[] = [];
-    let words: string[] = [];
-    const vowels: VowelSegment[] = AllSegments.getAllSegmentsHardcoded().filter(seg => seg instanceof VowelSegment); // dummy data
-    const consonants: ConsonantSegment[] = AllSegments.getAllSegmentsHardcoded().filter(seg => seg instanceof ConsonantSegment) as ConsonantSegment[];
+	let petalConsonants: Segment[] = [];
+	// Each array in syllable really represents a word.
+	let syllables: string[][] = [];
+	const vowels: VowelSegment[] = AllSegments.getAllSegmentsHardcoded().filter(
+		(seg) => seg instanceof VowelSegment,
+	); // dummy data
+	const consonants: ConsonantSegment[] =
+		AllSegments.getAllSegmentsHardcoded().filter(
+			(seg) =>
+				seg instanceof ConsonantSegment &&
+				seg.categories.includes(practiceTarget!),
+		) as ConsonantSegment[];
+	// TODO - listening practice has practiceTarget null! something about the line above has to be fixed.
 
+	if (segment === null) {
+		segment = getRandomElement(consonants)!;
+	}
 
-    if (segment === null) {
-        segment = getRandomElement(consonants)!;
-    }
+	if (segment instanceof VowelSegment) {
+		let randomConsonantSegment = getRandomElement(consonants);
+		petalConsonants =
+			randomConsonantSegment!.fetchConsonantSiblings(consonantFlower);
+	} else if (segment instanceof ConsonantSegment) {
+		petalConsonants = segment!.fetchConsonantSiblings(consonantFlower);
+	} else {
+		throw 'segment is neither Vowel or Consonant segments!';
+	}
 
-    if (segment instanceof VowelSegment) {
-        let randomConsonantSegment = getRandomElement(consonants);
-        petalConsonants = randomConsonantSegment!.fetchConsonantSiblings(consonantFlower);
-    } else if (segment instanceof ConsonantSegment) {
-        petalConsonants = segment!.fetchConsonantSiblings(consonantFlower);
-    } else {
-        throw "segment is neither Vowel or Consonant segments!";
-    }
+	if (segment instanceof VowelSegment) {
+		let randomConsonantSegment = getRandomElement(
+			consonants.filter(
+				(consonant) => consonant.getPetalIds(consonantFlower).length != 0,
+			),
+		);
+		syllables[0] = [randomConsonantSegment!.name, segment.name];
+		let petalConsonants =
+			randomConsonantSegment!.fetchConsonantSiblings(consonantFlower);
+		console.log('petalConsonats:', petalConsonants);
+		if (isVariegatedVowel) {
+			syllables[1] = [
+				getRandomElement(petalConsonants)!.name,
+				getRandomElement(
+					vowels.filter((vowel) => vowel.name !== segment!.name),
+				)!.name,
+			];
+		} else {
+			syllables[1] = [getRandomElement(petalConsonants)!.name, segment.name];
+		}
+	} else {
+		petalConsonants = petalConsonants.filter((segment) => {
+			let consonantSegment = segment as ConsonantSegment;
+			return consonantSegment.categories.includes(practiceTarget!);
+		});
+		console.log('petalConsoants:', petalConsonants);
 
-    if (segment instanceof VowelSegment) {
-        let randomConsonantSegment = getRandomElement(consonants.filter(consonant => consonant.getPetalIds(consonantFlower).length != 0));
-        words[0] = randomConsonantSegment!.name + segment.name;
-        let petalConsonants = randomConsonantSegment!.fetchConsonantSiblings(consonantFlower);
-        console.log("petalConsonats:", petalConsonants);
-        if (isVariegatedVowel) {
-            words[1] = getRandomElement(petalConsonants)!.name + getRandomElement(vowels.filter(vowel => vowel.name !== segment!.name))!.name; // TODO -- hopefully it actually filters it out!
-            console.log("words[1]:", words)
-        } else { 
-            words[1] = getRandomElement(petalConsonants)!.name + segment.name;
-        }
-    } else {
-        petalConsonants = petalConsonants.filter(segment => {
-            let consonantSegment = segment as ConsonantSegment;
-            return consonantSegment.categories.includes(practiceTarget!)});
-        console.log("petalConsoants:", petalConsonants);
+		// Store random vowel in case isVariegatedVowel is false.
+		let randomVowel = getRandomElement(vowels)!.name;
+		syllables[0] = [segment!.name, randomVowel];
+		if (isVariegatedVowel) {
+			syllables[1] = [
+				getRandomElement(petalConsonants)!.name,
+				getRandomElement(vowels.filter((vowel) => vowel.name !== randomVowel))!
+					.name,
+			];
+		} else {
+			syllables[1] = [getRandomElement(petalConsonants)!.name, randomVowel];
+		}
+	}
+	console.log('words:', syllables);
 
-        // Store random vowel in case isVariegatedVowel is false.
-        let randomVowel = getRandomElement(vowels)!.name;
-        words[0] = segment!.name + randomVowel;
-        console.log("words[0]:", words)
-        if (isVariegatedVowel) {
-            words[1] = getRandomElement(petalConsonants)!.name + getRandomElement(vowels.filter(vowel => vowel.name !== randomVowel))!.name; // TODO -- hopefully it actually filters it out!
-            console.log("words[1]:", words)
-        } else { 
-            words[1] = getRandomElement(petalConsonants)!.name + randomVowel;
-        }
-    }
-    console.log("words:", words)
-    return words;
+	let words: string[] = [];
+	if (practiceTarget == ConsonantCategories.Initial) {
+		words = syllables.map((wordArray) => wordArray[0] + wordArray[1]);
+	} else {
+		words = syllables.map((wordArray) => wordArray[1] + wordArray[0]);
+	}
+
+	return words;
 }
 
 // Example usage:
@@ -68,9 +103,9 @@ export function syllableGeneration(
 // const randomElement = getRandomElement(myArray);
 // console.log(randomElement); // This will log a random element from myArray
 function getRandomElement<T>(array: T[]): T | undefined {
-    if (array.length === 0) return undefined;
-    const randomIndex = Math.floor(Math.random() * array.length);
-    return array[randomIndex];
+	if (array.length === 0) return undefined;
+	const randomIndex = Math.floor(Math.random() * array.length);
+	return array[randomIndex];
 }
 
 // Testing:
@@ -88,12 +123,13 @@ function getRandomElement<T>(array: T[]): T | undefined {
 //     )
 // )
 
-console.log("result:",
-    syllableGeneration(
-        new VowelSegment('oo'),
-        ConsonantFlower.Manner,
-        true,
-        ConsonantCategories.Initial,
-        2
-    )
-)
+console.log(
+	'result:',
+	syllableGeneration(
+		new VowelSegment('oo'),
+		ConsonantFlower.Manner,
+		true,
+		ConsonantCategories.Final,
+		2,
+	),
+);
