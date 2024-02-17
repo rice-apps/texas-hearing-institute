f4730bc75a81862f61aacfe151749d8065925fd2
Chloe Lim <chloelim608@gmail.com>
2024-02-17 15:55:54 -0600

    version 2 changes

--- texas-hearing-institute/App.tsx
+++ texas-hearing-institute/App.tsx
@@ -2,16 +2,27 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Swiper from 'react-native-deck-swiper';
+import { useState } from 'react';
 
 
 const App = () => {
-  const cards = ['lol', 'lmao', 'rofl', 'wtf', 'omw'];
+  const cards = ['lol', 'lmao', 'rofl', 'wtf', 'omw', 'ngl','tbh'];
+  const [isSwipedRight, setIsSwipedRight] = useState(false);
+  const [isSwipedLeft, setIsSwipedLeft] = useState(false);
+
+  const handleOnSwipedRight = () => {
+    setIsSwipedRight(true);
+  };
+  const handleOnSwipedLeft = () => {
+    setIsSwipedLeft(true);
+  }
   return (
-    <View style={styles.container}> 
+    <View style={styles.container}>
       <Swiper
         cards={cards}
         renderCard={(card) => (
-          <View style={styles.card}>
+          <View style={[styles.card, isSwipedRight && styles.greenBorder, 
+          isSwipedLeft && styles.yellowBorder]}>
             <Text style={styles.text}>{card}</Text>
           </View>
 
@@ -22,14 +33,18 @@ const App = () => {
         onSwipedAll={() => {
           console.log('All cards have been swiped');
         }}
-        cardIndex={0}
-        backgroundColor="#4FD0E9"
+        // onSwipedRight={(cardIndex)=> {
+        //   console.log(`Swiped card index: ${cardIndex} RIGHT`)
+        // }}
+        onSwipedRight={handleOnSwipedRight}
+        onSwipedLeft={handleOnSwipedLeft}
+        //cardIndex={0}
+        backgroundColor="#F5F5F5" // background outside card
         stackSize={3}
       >
       </Swiper>
-      <Text>Swipe right to mark <Text style={{ fontWeight: 'bold', color: '#A4C639' }}>CORRECT</Text></Text>
-      <Text></Text>
-      <Text>Swipe left to mark <Text style={{ fontWeight: 'bold', color: 'red' }}>STILL LEARNING</Text></Text>
+      <Text>Swipe right to mark <Text style={{ fontWeight: 'bold', color: '#34A853' }}>CORRECT</Text></Text>
+      <Text>Swipe left to mark <Text style={{ fontWeight: 'bold', color: '#FBBC05' }}>STILL LEARNING</Text></Text>
 
     </View>
   );
@@ -43,17 +58,27 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   card: {
+    flex: 0.4,
     height: 200,
     backgroundColor: 'white',
     borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    
     borderWidth: 2,
     borderColor: 'lightgray',
+    // width: 200,
+    // length: 250,
+    //aspectRatio:0.9,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
     fontSize: 30,
-    //fontWeight: 'bold',
+  },
+  greenBorder: {
+    borderColor: '#34A853', // Change border color to green when swipe right
+  },
+  yellowBorder: {
+    borderColor: '#FBBC05', // Change border color to yellow when swipe left
   },
 });
 
