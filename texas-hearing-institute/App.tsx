d9cb038290b8c62e7c5b534e4948db313138f404
Baylee Keevan <jade@baylees-laptop.dyndns.rice.edu>
2024-02-17 15:55:54 -0600

    added temp navbar

--- texas-hearing-institute/App.tsx
+++ texas-hearing-institute/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { ApplicationProvider } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
-import PracticeNavigator from './pages/PracticeNavigator';
+import TabNavigator from './components/TabNavigator';
 
 export default function App() {
 	return (
 		<ApplicationProvider {...eva} theme={eva.light}>
 			<StatusBar barStyle="dark-content" />
-			<PracticeNavigator />
+			<TabNavigator />
 		</ApplicationProvider>
 	);
 }
