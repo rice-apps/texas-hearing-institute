81a56332f409219f5c6720a961505e0234ab9597
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2024-02-17 14:35:21 -0600

    App.tsx REALLY wants to use tabs

--- texas-hearing-institute/App.tsx
+++ texas-hearing-institute/App.tsx
@@ -4,21 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text style={tw`text-3xl font-bold underline`}>
-                Open up App.tsx to start working on your app!
-            </Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+	return (
+		<View style={styles.container}>
+			<Text style={tw`text-3xl font-bold underline`}>
+				Open up App.tsx to start working on your app!
+			</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
