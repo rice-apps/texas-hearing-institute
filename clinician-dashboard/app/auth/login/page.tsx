49db7fe82948d76f42b613cd2296e0025f46d612
Pallob paul <pallobpaul366@gmail.com>
2024-02-17 15:59:02 -0600

    implemented microsoft login

--- clinician-dashboard/app/auth/login/page.tsx
+++ clinician-dashboard/app/auth/login/page.tsx
@@ -7,6 +7,9 @@ export default function Login() {
             <a href="/auth/google">
                 Sign in with Google
             </a>
+            <a href="/auth/azure">
+                Sign in with Azure
+            </a>2
         </div>
     );
 };
