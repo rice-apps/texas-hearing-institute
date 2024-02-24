570b40ded96ff6df9c40ee8bf4ef06454cf6dc67
Vincent Wang <51209452+vincentxwang@users.noreply.github.com>
2024-02-24 14:11:01 -0600

    syllable generation scaffolding (#34)
    * Create Segment
    
    * Segment defaults
    
    * Add ConsonantFlower and related methods
    
    * Add fetchConsonantSiblings
    
    * Example of allSegmentsHardcoded
    
    * More of allSegmentsHardcoded
    
    * ALL allSegmentsHardcoded — there may be errors
    
    * Segment object refactoring + comments
    
    * syllable generation sketch
    
    * Fix VowelSegment not defined
    
    * - Revert App.tsx
    - Fix fetchConsonantSiblings including our own segment
    - Fix filtering out all petalConsonants rather than just those which are not target practice category
    
    * fixed bug
    
    * fixed bug
    
    * added final consonants
    
    * added final consonants to syllable generation
    
    * Frog changes
    
    * added ConsonantFlower.All
    
    * added all attribute
    
    * added test cases + fixed bug with generating consonant
    
    * Improve documentation
    
    * App.tsx REALLY wants to use tabs
    
    * typo fix
    
    * Add some documentation
    
    * Finish syllableGeneration documentation
    
    * debug feature
    
    * Revert App.tsx
    
    * todo
    
    ---------
    
    Co-authored-by: CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
    Co-authored-by: Baylee Keevan <jade@baylees-laptop.dyndns.rice.edu>

--- texas-hearing-institute/components/Auth.native.tsx
+++ texas-hearing-institute/components/Auth.native.tsx
@@ -1,45 +1,47 @@
 import {
-    GoogleSignin,
-    GoogleSigninButton,
-    statusCodes,
-  } from '@react-native-google-signin/google-signin'
-  import { supabase } from '../lib/supabase'
-  
-  export default function Auth() {
-    GoogleSignin.configure({
-      scopes: ['https://www.googleapis.com/auth/drive.readonly'],
-      webClientId: '238625413111-ps04v28r5o19v7lssabudkr123cjtnrf.apps.googleusercontent.com',
-    })
-  
-    return (
-      <GoogleSigninButton
-        size={GoogleSigninButton.Size.Wide}
-        color={GoogleSigninButton.Color.Dark}
-        onPress={async () => {
-          try {
-            await GoogleSignin.hasPlayServices()
-            const userInfo = await GoogleSignin.signIn()
-            if (userInfo.idToken) {
-              const { data, error } = await supabase.auth.signInWithIdToken({
-                provider: 'google',
-                token: userInfo.idToken,
-              })
-              console.log(error, data)
-            } else {
-              throw new Error('no ID token present!')
-            }
-          } catch (error: any) {
-            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-              // user cancelled the login flow
-            } else if (error.code === statusCodes.IN_PROGRESS) {
-              // operation (e.g. sign in) is in progress already
-            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-              // play services not available or outdated
-            } else {
-              // some other error happened
-            }
-          }
-        }}
-      />
-    )
-  }
+	GoogleSignin,
+	GoogleSigninButton,
+	statusCodes,
+} from '@react-native-google-signin/google-signin';
+import { supabase } from '../lib/supabase';
+
+export default function Auth() {
+	GoogleSignin.configure({
+		scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+		webClientId:
+			'238625413111-ps04v28r5o19v7lssabudkr123cjtnrf.apps.googleusercontent.com',
+	});
+
+	return (
+		<GoogleSigninButton
+			size={GoogleSigninButton.Size.Wide}
+			color={GoogleSigninButton.Color.Dark}
+			onPress={async () => {
+				try {
+					await GoogleSignin.hasPlayServices();
+					const userInfo = await GoogleSignin.signIn();
+					if (userInfo.idToken) {
+						const { data, error } = await supabase.auth.signInWithIdToken({
+							provider: 'google',
+							token: userInfo.idToken,
+						});
+						console.log(error, data);
+					} else {
+						throw new Error('no ID token present!');
+					}
+					// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				} catch (error: any) {
+					if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+						// user cancelled the login flow
+					} else if (error.code === statusCodes.IN_PROGRESS) {
+						// operation (e.g. sign in) is in progress already
+					} else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+						// play services not available or outdated
+					} else {
+						// some other error happened
+					}
+				}
+			}}
+		/>
+	);
+}
--- texas-hearing-institute/lib/supabase.ts
+++ texas-hearing-institute/lib/supabase.ts
@@ -1,19 +1,19 @@
-import { AppState } from 'react-native'
-import 'react-native-url-polyfill/auto'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient } from '@supabase/supabase-js'
+import { AppState } from 'react-native';
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl!, supabaseAnonKey!, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-})
+	auth: {
+		storage: AsyncStorage,
+		autoRefreshToken: true,
+		persistSession: true,
+		detectSessionInUrl: false,
+	},
+});
 
 // Tells Supabase Auth to continuously refresh the session automatically
 // if the app is in the foreground. When this is added, you will continue
@@ -21,9 +21,9 @@ export const supabase = createClient(supabaseUrl!, supabaseAnonKey!, {
 // `SIGNED_OUT` event if the user's session is terminated. This should
 // only be registered once.
 AppState.addEventListener('change', (state) => {
-  if (state === 'active') {
-    supabase.auth.startAutoRefresh()
-  } else {
-    supabase.auth.stopAutoRefresh()
-  }
-})
+	if (state === 'active') {
+		supabase.auth.startAutoRefresh();
+	} else {
+		supabase.auth.stopAutoRefresh();
+	}
+});
