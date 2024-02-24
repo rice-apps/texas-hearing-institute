02e62b3e725f90fc1ec90dc543d9dfd371d118d8
Pallob paul <pallobpaul366@gmail.com>
2024-02-24 14:12:50 -0600

    login completed

--- clinician-dashboard/app/auth/login/page.tsx
+++ clinician-dashboard/app/auth/login/page.tsx
@@ -4,12 +4,9 @@ export default function Login() {
 
     return (
         <div>
-            <a href="/auth/google">
-                Sign in with Google
-            </a>
             <a href="/auth/azure">
                 Sign in with Azure
-            </a>2
+            </a>
         </div>
     );
 };
