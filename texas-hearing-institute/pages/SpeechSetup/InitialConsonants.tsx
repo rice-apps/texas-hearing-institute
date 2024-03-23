4b2f417e93e9a4c021695e0fbf6886f5b1bb8f2b
Chloe Lim <chloelim608@gmail.com>
2024-03-23 13:14:06 -0500

    SpeechSetup files

---
+++ texas-hearing-institute/pages/SpeechSetup/InitialConsonants.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ScrollView, View } from 'react-native';
+import Heading from '../../components/Heading';
+import Subheading from '../../components/SubHeading';
+// import GridButton from '../../components/GridButton';
+import Selection from '../../components/Selection';
+import BigButton from '../../components/Button';
+import { Alert } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+
+const InitialConsonants = () => {
+	const showAlert = () => {
+		Alert.alert('Come later', 'We are working on it, bruh!', [
+			{
+				text: 'Cancel',
+				style: 'cancel', // This button will be the 'Cancel' button
+			},
+			{ text: 'OK' }, // This will be the 'OK' button
+		]);
+	};
+
+	return (
+		<ScrollView>
+			<Heading title={'Initial Consonants'}></Heading>
+			<Subheading
+				title={'Select a consonant and blahblah to blahblah practise speaking'}
+			></Subheading>
+			{/* <GridButton/> */}
+			<Selection
+				buttonNames={['Variegated Vowels', 'Voicing', 'Manner', 'Place Cue']}
+			></Selection>
+			<View style={tw`p-4`}></View>
+			<BigButton label={'Submit'} onPress={showAlert} />
+		</ScrollView>
+	);
+};
+
+export default InitialConsonants;
