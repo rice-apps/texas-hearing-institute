44ef4e2b7814a31c5d74bf45e3ce543be01befab
Chloe Lim <chloelim608@gmail.com>
2024-03-23 13:42:58 -0500

    3/23/

--- texas-hearing-institute/components/FormView/FormView.tsx
+++ texas-hearing-institute/components/FormView/FormView.tsx
@@ -1,130 +1,130 @@
-import React from 'react';
-import { View, Text, TextInput } from 'react-native';
-import {
-	getKeyboardType,
-	getTextContentType,
-	TextProperties,
-} from '../../utils/TextProperties';
-import Subheading from '../Subheading';
+// import React from 'react';
+// import { View, Text, TextInput } from 'react-native';
+// import {
+// 	getKeyboardType,
+// 	getTextContentType,
+// 	TextProperties,
+// } from '../../utils/TextProperties';
+// import Subheading from '../SubHeading';
 
-interface RichText {
-	text: string;
-	properties?: TextProperties;
-}
+// interface RichText {
+// 	text: string;
+// 	properties?: TextProperties;
+// }
 
-interface FormViewProps {
-	heading?: string;
-	labels: string[];
-	data: RichText[];
-	readonly: boolean;
-	setData: ((newValue: string) => void)[];
-}
+// interface FormViewProps {
+// 	heading?: string;
+// 	labels: string[];
+// 	data: RichText[];
+// 	readonly: boolean;
+// 	setData: ((newValue: string) => void)[];
+// }
 
-const FormView = ({
-	heading,
-	labels,
-	data,
-	readonly,
-	setData,
-}: FormViewProps) => {
-	if (
-		labels.length !== data.length ||
-		(labels.length !== setData.length && !readonly)
-	) {
-		return (
-			<View
-				style={{
-					width: '100%',
-					backgroundColor: 'red',
-					padding: 15,
-					borderRadius: 10,
-				}}
-			>
-				<Text
-					style={{
-						color: 'white',
-						textAlign: 'center',
-						fontWeight: '700',
-					}}
-				>
-					Error: Label length ({labels.length}) does not match data length (
-					{data.length}) or does not match set data length ({setData.length})
-				</Text>
-			</View>
-		);
-	}
+// const FormView = ({
+// 	heading,
+// 	labels,
+// 	data,
+// 	readonly,
+// 	setData,
+// }: FormViewProps) => {
+// 	if (
+// 		labels.length !== data.length ||
+// 		(labels.length !== setData.length && !readonly)
+// 	) {
+// 		return (
+// 			<View
+// 				style={{
+// 					width: '100%',
+// 					backgroundColor: 'red',
+// 					padding: 15,
+// 					borderRadius: 10,
+// 				}}
+// 			>
+// 				<Text
+// 					style={{
+// 						color: 'white',
+// 						textAlign: 'center',
+// 						fontWeight: '700',
+// 					}}
+// 				>
+// 					Error: Label length ({labels.length}) does not match data length (
+// 					{data.length}) or does not match set data length ({setData.length})
+// 				</Text>
+// 			</View>
+// 		);
+// 	}
 
-	return (
-		<View /* Main container */
-			style={{
-				flexDirection: 'column',
-				alignItems: 'flex-start',
-				width: '100%',
-			}}
-		>
-			{heading == null ? null : (
-				<View
-					style={{
-						marginLeft: 10,
-						marginBottom: 16,
-					}}
-				>
-					<Subheading title={heading} />
-				</View>
-			)}
-			<View /* Main Form Content */
-				style={{
-					flexDirection: 'column',
-					borderRadius: 12,
-					borderStyle: 'solid',
-					borderColor: 'rgba(217, 217, 217, 0.50)',
-					borderWidth: 2,
-					width: '100%',
-				}}
-			>
-				{labels.map((item, index) => (
-					<View /* Each row in the form */ key={index}>
-						<View /* the row itself */
-							style={{
-								height: 48,
-								marginHorizontal: 16,
-								flexDirection: 'row',
-								alignItems: 'center',
-								justifyContent: 'space-between',
-							}}
-						>
-							<Text
-								style={{
-									color: '#333',
-									fontSize: 16,
-									fontWeight: '500',
-								}}
-							>
-								{labels[index]}
-							</Text>
-							<TextInput
-								secureTextEntry={data[index].properties?.password === true}
-								value={data[index].text}
-								onChangeText={(newValue) => {
-									setData[index](newValue);
-								}}
-								placeholder={data[index].properties?.placeholder}
-								editable={
-									!readonly && !(data[index].properties?.readonly === true)
-								}
-								keyboardType={getKeyboardType(data[index].properties)}
-								textContentType={getTextContentType(data[index].properties)}
-								style={{
-									width: 170,
-									textAlign: 'right',
-								}}
-							/>
-						</View>
-					</View>
-				))}
-			</View>
-		</View>
-	);
-};
+// 	return (
+// 		<View /* Main container */
+// 			style={{
+// 				flexDirection: 'column',
+// 				alignItems: 'flex-start',
+// 				width: '100%',
+// 			}}
+// 		>
+// 			{heading == null ? null : (
+// 				<View
+// 					style={{
+// 						marginLeft: 10,
+// 						marginBottom: 16,
+// 					}}
+// 				>
+// 					<Subheading title={heading} />
+// 				</View>
+// 			)}
+// 			<View /* Main Form Content */
+// 				style={{
+// 					flexDirection: 'column',
+// 					borderRadius: 12,
+// 					borderStyle: 'solid',
+// 					borderColor: 'rgba(217, 217, 217, 0.50)',
+// 					borderWidth: 2,
+// 					width: '100%',
+// 				}}
+// 			>
+// 				{labels.map((item, index) => (
+// 					<View /* Each row in the form */ key={index}>
+// 						<View /* the row itself */
+// 							style={{
+// 								height: 48,
+// 								marginHorizontal: 16,
+// 								flexDirection: 'row',
+// 								alignItems: 'center',
+// 								justifyContent: 'space-between',
+// 							}}
+// 						>
+// 							<Text
+// 								style={{
+// 									color: '#333',
+// 									fontSize: 16,
+// 									fontWeight: '500',
+// 								}}
+// 							>
+// 								{labels[index]}
+// 							</Text>
+// 							<TextInput
+// 								secureTextEntry={data[index].properties?.password === true}
+// 								value={data[index].text}
+// 								onChangeText={(newValue) => {
+// 									setData[index](newValue);
+// 								}}
+// 								placeholder={data[index].properties?.placeholder}
+// 								editable={
+// 									!readonly && !(data[index].properties?.readonly === true)
+// 								}
+// 								keyboardType={getKeyboardType(data[index].properties)}
+// 								textContentType={getTextContentType(data[index].properties)}
+// 								style={{
+// 									width: 170,
+// 									textAlign: 'right',
+// 								}}
+// 							/>
+// 						</View>
+// 					</View>
+// 				))}
+// 			</View>
+// 		</View>
+// 	);
+// };
 
-export default FormView;
+// export default FormView;
--- texas-hearing-institute/pages/Home/Home.tsx
+++ texas-hearing-institute/pages/Home/Home.tsx
@@ -1,161 +1,161 @@
-import { StatusBar } from 'expo-status-bar';
-import {
-	Pressable,
-	StyleSheet,
-	Text,
-	TouchableOpacity,
-	View,
-	Button,
-} from 'react-native';
-import Constants from 'expo-constants';
-//import { Button } from 'react-native-elements';
-import tw from 'tailwind-react-native-classnames';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import * as Speech from 'expo-speech';
-import InitialConsonants from '../SpeechSetup/InitialConsonants';
-import { StackNavigationProp } from '@react-navigation/stack';
-import ListeningNavigator from '../listening-settings/ListeningNavigator';
-import { RootStackParamList } from './types';
+// import { StatusBar } from 'expo-status-bar';
+// import {
+// 	Pressable,
+// 	StyleSheet,
+// 	Text,
+// 	TouchableOpacity,
+// 	View,
+// 	Button,
+// } from 'react-native';
+// import Constants from 'expo-constants';
+// //import { Button } from 'react-native-elements';
+// import tw from 'tailwind-react-native-classnames';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+// import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+// import * as Speech from 'expo-speech';
+// import InitialConsonants from '../SpeechSetup/InitialConsonants';
+// import { StackNavigationProp } from '@react-navigation/stack';
+// import ListeningNavigator from '../ListeningSettings/ListeningNavigator';
+// import { RootStackParamList } from './types';
 
-// import PracticeCard from './components/PracticeCard';
-// import SpeechNavigation from './pages/SpeechSetup/SpeechNavigation';
+// // import PracticeCard from './components/PracticeCard';
+// // import SpeechNavigation from './pages/SpeechSetup/SpeechNavigation';
 
-// import ProgressBar from "../Progress";
-/*
-TODO: custom audio button
-link to consonants page
-text-to-speech
-track correct/incorrect answers
-*/
+// // import ProgressBar from "../Progress";
+// /*
+// TODO: custom audio button
+// link to consonants page
+// text-to-speech
+// track correct/incorrect answers
+// */
 
-type ProfileScreenNavigationProp = StackNavigationProp<
-	RootStackParamList,
-	'Home'
->;
+// type ProfileScreenNavigationProp = StackNavigationProp<
+// 	RootStackParamList,
+// 	'Home'
+// >;
 
-type Props = {
-	navigation: ProfileScreenNavigationProp;
-};
+// type Props = {
+// 	navigation: ProfileScreenNavigationProp;
+// };
 
-const Home = ({ navigation }: Props) => {
-	return (
-		<View
-			style={[
-				styles.container,
-				{
-					flexDirection: 'column',
-				},
-			]}
-		>
-			<Text style={tw`text-3xl font-bold pt-10`}>Good morning Bob!</Text>
-			<Text style={tw`text-base pt-6`}>Let's get practicing!</Text>
+// const Home = ({ navigation }: Props) => {
+// 	return (
+// 		<View
+// 			style={[
+// 				styles.container,
+// 				{
+// 					flexDirection: 'column',
+// 				},
+// 			]}
+// 		>
+// 			<Text style={tw`text-3xl font-bold pt-10`}>Good morning Bob!</Text>
+// 			<Text style={tw`text-base pt-6`}>Let's get practicing!</Text>
 
-			<Icon style={tw`p-5`} name="circle" size={25} color="black">
-				<Text style={tw`text-2xl font-bold pt-6`}> Speech Babble</Text>
-			</Icon>
+// 			<Icon style={tw`p-5`} name="circle" size={25} color="black">
+// 				<Text style={tw`text-2xl font-bold pt-6`}> Speech Babble</Text>
+// 			</Icon>
 
-			<Pressable
-				style={({ pressed }) => [
-					styles.box,
-					pressed ? styles.pressed : styles.box,
-				]}
-				onPress={() => navigation.navigate('InitialConsonants')}
-			>
-				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Vowels</Text>
-			</Pressable>
+// 			<Pressable
+// 				style={({ pressed }) => [
+// 					styles.box,
+// 					pressed ? styles.pressed : styles.box,
+// 				]}
+// 				onPress={() => navigation.navigate('InitialConsonants')}
+// 			>
+// 				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Vowels</Text>
+// 			</Pressable>
 
-			<Pressable
-				style={({ pressed }) => [
-					styles.box,
-					pressed ? styles.pressed : styles.box,
-				]}
-				onPress={() => navigation.navigate('InitialConsonants')}
-			>
-				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Initial Consonants</Text>
-			</Pressable>
-			<Pressable
-				style={({ pressed }) => [
-					styles.box,
-					pressed ? styles.pressed : styles.box,
-				]}
-				onPress={() => navigation.navigate('InitialConsonants')}
-			>
-				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Final Consonants</Text>
-			</Pressable>
+// 			<Pressable
+// 				style={({ pressed }) => [
+// 					styles.box,
+// 					pressed ? styles.pressed : styles.box,
+// 				]}
+// 				onPress={() => navigation.navigate('InitialConsonants')}
+// 			>
+// 				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Initial Consonants</Text>
+// 			</Pressable>
+// 			<Pressable
+// 				style={({ pressed }) => [
+// 					styles.box,
+// 					pressed ? styles.pressed : styles.box,
+// 				]}
+// 				onPress={() => navigation.navigate('InitialConsonants')}
+// 			>
+// 				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Final Consonants</Text>
+// 			</Pressable>
 
-			<Icon style={tw`p-5`} name="circle" size={25} color="black">
-				<Text style={tw`text-2xl font-bold pt-6 mx-5 mt-1`}>
-					{' '}
-					Listening Babble
-				</Text>
-			</Icon>
-			<Pressable
-				style={({ pressed }) => [
-					styles.box,
-					pressed ? styles.pressed : styles.box,
-				]}
-				onPress={() => navigation.navigate('VariegatedVowels')}
-			>
-				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Variegated Vowels</Text>
-			</Pressable>
-			<Pressable
-				style={({ pressed }) => [
-					styles.box,
-					pressed ? styles.pressed : styles.box,
-				]}
-				onPress={() => navigation.navigate('Voicing')}
-			>
-				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Voicing</Text>
-			</Pressable>
-			<Pressable
-				style={({ pressed }) => [
-					styles.box,
-					pressed ? styles.pressed : styles.box,
-				]}
-				onPress={() => navigation.navigate('Manner')}
-			>
-				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Manner</Text>
-			</Pressable>
-			<Pressable
-				style={({ pressed }) => [
-					styles.box,
-					pressed ? styles.pressed : styles.box,
-				]}
-				onPress={() => navigation.navigate('PlaceCueTab')}
-			>
-				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Place Cue</Text>
-			</Pressable>
+// 			<Icon style={tw`p-5`} name="circle" size={25} color="black">
+// 				<Text style={tw`text-2xl font-bold pt-6 mx-5 mt-1`}>
+// 					{' '}
+// 					Listening Babble
+// 				</Text>
+// 			</Icon>
+// 			<Pressable
+// 				style={({ pressed }) => [
+// 					styles.box,
+// 					pressed ? styles.pressed : styles.box,
+// 				]}
+// 				onPress={() => navigation.navigate('VariegatedVowels')}
+// 			>
+// 				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Variegated Vowels</Text>
+// 			</Pressable>
+// 			<Pressable
+// 				style={({ pressed }) => [
+// 					styles.box,
+// 					pressed ? styles.pressed : styles.box,
+// 				]}
+// 				onPress={() => navigation.navigate('Voicing')}
+// 			>
+// 				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Voicing</Text>
+// 			</Pressable>
+// 			<Pressable
+// 				style={({ pressed }) => [
+// 					styles.box,
+// 					pressed ? styles.pressed : styles.box,
+// 				]}
+// 				onPress={() => navigation.navigate('Manner')}
+// 			>
+// 				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Manner</Text>
+// 			</Pressable>
+// 			<Pressable
+// 				style={({ pressed }) => [
+// 					styles.box,
+// 					pressed ? styles.pressed : styles.box,
+// 				]}
+// 				onPress={() => navigation.navigate('PlaceCueTab')}
+// 			>
+// 				<Text style={tw`text-lg pt-2 pl-2 mx-5 mt-1`}>Place Cue</Text>
+// 			</Pressable>
 
-			<StatusBar style="auto" />
-		</View>
+// 			<StatusBar style="auto" />
+// 		</View>
 
-		//<View style={styles.container}><Text>Hello world</Text></View>
-		////   <Text>Speech Babble</Text>
-		//<Button>button</Button>
-		//
-	);
-};
+// 		//<View style={styles.container}><Text>Hello world</Text></View>
+// 		////   <Text>Speech Babble</Text>
+// 		//<Button>button</Button>
+// 		//
+// 	);
+// };
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		//alignItems: 'center',
-		justifyContent: 'space-between',
-		padding: 20,
-	},
-	box: {
-		flex: 0.25,
-		backgroundColor: '#e3dfde',
-		justifyContent: 'space-between',
-		//alignItems: 'center',
-		marginTop: 20,
-		borderRadius: 20,
-	},
-	pressed: {
-		backgroundColor: '#a19e9d',
-	},
-});
-export default Home;
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: '#fff',
+// 		//alignItems: 'center',
+// 		justifyContent: 'space-between',
+// 		padding: 20,
+// 	},
+// 	box: {
+// 		flex: 0.25,
+// 		backgroundColor: '#e3dfde',
+// 		justifyContent: 'space-between',
+// 		//alignItems: 'center',
+// 		marginTop: 20,
+// 		borderRadius: 20,
+// 	},
+// 	pressed: {
+// 		backgroundColor: '#a19e9d',
+// 	},
+// });
+// export default Home;
