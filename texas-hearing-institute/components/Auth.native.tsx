edc77275a16cb75ba2e4685beda997ce48b1c4a2
Shivam Pathak <shivampathak108@outlook.com>
2024-04-15 17:04:17 -0500

    implemented hashed sign in

--- texas-hearing-institute/components/Auth.native.tsx
+++ texas-hearing-institute/components/Auth.native.tsx
@@ -1,21 +1,20 @@
-import {
-	GoogleSignin,
-	GoogleSigninButton,
-	statusCodes,
-} from '@react-native-google-signin/google-signin';
-import { View, Platform } from 'react-native';
-import * as AppleAuthentication from 'expo-apple-authentication';
+import React, { useState } from 'react';
 import { supabase } from '../lib/supabase';
+import { Alert, StyleSheet, View, AppState } from 'react-native';
+import { Button, Input } from 'react-native-elements';
+import CryptoJS from 'crypto-es';
+import { Platform } from 'react-native';
+import * as AppleAuthentication from 'expo-apple-authentication';
+
+AppState.addEventListener('change', (state) => {
+	if (state === 'active') {
+		supabase.auth.startAutoRefresh();
+	} else {
+		supabase.auth.stopAutoRefresh();
+	}
+});
 
 export default function Auth() {
-	GoogleSignin.configure({
-		scopes: ['https://www.googleapis.com/auth/drive.readonly'],
-		webClientId:
-			'238625413111-ps04v28r5o19v7lssabudkr123cjtnrf.apps.googleusercontent.com',
-		iosClientId:
-			'238625413111-n13uhletv4i0q5b85kvat11jcp1e11la.apps.googleusercontent.com',
-	});
-
 	if (Platform.OS === 'ios') {
 		return (
 			<View>
@@ -57,74 +56,121 @@ export default function Auth() {
 						}
 					}}
 				/>
-				<GoogleSigninButton
-					size={GoogleSigninButton.Size.Wide}
-					color={GoogleSigninButton.Color.Dark}
-					onPress={async () => {
-						try {
-							await GoogleSignin.hasPlayServices();
-							const userInfo = await GoogleSignin.signIn();
-							if (userInfo.idToken) {
-								const { data, error } = await supabase.auth.signInWithIdToken({
-									provider: 'google',
-									token: userInfo.idToken,
-								});
-								console.log(error, data);
-							} else {
-								throw new Error('no ID token present!');
-							}
-							//
-							// eslint-disable-next-line @typescript-eslint/no-explicit-any
-						} catch (error: any) {
-							if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-								// user cancelled the login flow
-							} else if (error.code === statusCodes.IN_PROGRESS) {
-								// operation (e.g. sign in) is in progress already
-							} else if (
-								error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE
-							) {
-								// play services not available or outdated
-							} else {
-								// some other error happened
-							}
-						}
-					}}
-				/>
 			</View>
 		);
 	}
 
+	const [email, setEmail] = useState('');
+	const [encryptedEmail, setEncryptedEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [loading, setLoading] = useState(false);
+
+	function emailSetter(email: string) {
+		setEmail(email);
+		if (email.indexOf('@') > 0) {
+			let part1 = email.substring(0, email.indexOf('@'));
+			const part2 = email.substring(email.indexOf('@'));
+			part1 = CryptoJS.SHA256(part1).toString();
+			setEncryptedEmail(part1 + part2);
+		}
+	}
+
+	async function checkExists(encrypted: string) {
+		const { data, error } = await supabase
+			.from('users')
+			.select('email')
+			.eq('email', encrypted);
+
+		if (error) {
+			throw error;
+		}
+
+		return data.length > 0;
+	}
+
+	async function signInWithEmail() {
+		setLoading(true);
+		const flag = await checkExists(encryptedEmail);
+		if (!flag) {
+			Alert.alert('This user does not exist');
+			setLoading(false);
+			return;
+		}
+		const { error } = await supabase.auth.signInWithPassword({
+			email: encryptedEmail,
+			password: password,
+		});
+
+		if (error) Alert.alert(error.message);
+		setLoading(false);
+	}
+	async function signUpWithEmail() {
+		setLoading(true);
+		const flag = await checkExists(encryptedEmail);
+		if (flag) {
+			Alert.alert('This user already exists');
+			setLoading(false);
+			return;
+		}
+		const { error: error } = await supabase.auth.signUp({
+			email: encryptedEmail,
+			password: password,
+		});
+
+		if (error) Alert.alert(error.message);
+		setLoading(false);
+	}
+	const styles = StyleSheet.create({
+		container: {
+			marginTop: 40,
+			padding: 12,
+		},
+		verticallySpaced: {
+			paddingTop: 4,
+			paddingBottom: 4,
+			alignSelf: 'stretch',
+		},
+		mt20: {
+			marginTop: 20,
+		},
+	});
 	return (
-		<GoogleSigninButton
-			size={GoogleSigninButton.Size.Wide}
-			color={GoogleSigninButton.Color.Dark}
-			onPress={async () => {
-				try {
-					await GoogleSignin.hasPlayServices();
-					const userInfo = await GoogleSignin.signIn();
-					if (userInfo.idToken) {
-						const { data, error } = await supabase.auth.signInWithIdToken({
-							provider: 'google',
-							token: userInfo.idToken,
-						});
-						console.log(error, data);
-					} else {
-						throw new Error('no ID token present!');
-					}
-					//
-					// eslint-disable-next-line @typescript-eslint/no-explicit-any
-				} catch (error: any) {
-					if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-						// user cancelled the login flow
-					} else if (error.code === statusCodes.IN_PROGRESS) {
-						// operation (e.g. sign in) is in progress already
-					} else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-						// play services not available or outdated
-					} else {
-						// some other error happened
-					}
-				}
-			}}
-		/>
+		<View style={styles.container}>
+			<View style={[styles.verticallySpaced, styles.mt20]}>
+				<Input
+					label="Email"
+					leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+					onChangeText={(text) => emailSetter(text)}
+					value={email}
+					placeholder="email@address.com"
+					autoCapitalize={'none'}
+				/>
+			</View>
+			<View style={styles.verticallySpaced}>
+				<Input
+					label="Password"
+					leftIcon={{ type: 'font-awesome', name: 'lock' }}
+					onChangeText={(text) => setPassword(text)}
+					value={password}
+					secureTextEntry={true}
+					placeholder="Password"
+					autoCapitalize={'none'}
+				/>
+			</View>
+			<View style={[styles.verticallySpaced, styles.mt20]}>
+				<Button
+					title="Sign in"
+					disabled={loading}
+					onPress={() => signInWithEmail()}
+				/>
+			</View>
+			<View style={styles.verticallySpaced}>
+				<Button
+					title="Sign up"
+					disabled={loading}
+					onPress={() => signUpWithEmail()}
+				/>
+			</View>
+		</View>
 	);
 }
