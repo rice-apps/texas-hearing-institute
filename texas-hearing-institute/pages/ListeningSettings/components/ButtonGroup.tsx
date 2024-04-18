c2bae3b44b8972e4aeaca36ff4695d300cee1079
Chloe Lim <chloelim608@gmail.com>
2024-04-17 21:48:27 -0500

    white background

--- texas-hearing-institute/pages/ListeningSettings/components/ButtonGroup.tsx
+++ texas-hearing-institute/pages/ListeningSettings/components/ButtonGroup.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ViewStyle } from 'react-native';
 import SettingsButton from './SettingsButton';
 
 interface ButtonGroupProps {
 	headerText: string;
 	buttonLabels: string[];
 	buttonRoutes: string[];
+	buttonStyle?: ViewStyle;
 }
 
 export default function ButtonGroup({
 	headerText,
 	buttonLabels,
 	buttonRoutes,
+	buttonStyle,
 }: ButtonGroupProps) {
 	return (
 		<View style={styles.container}>
@@ -19,7 +21,13 @@ export default function ButtonGroup({
 				<Text style={styles.headerText}>{headerText}</Text>
 			</View>
 			{buttonLabels.map((label: string, indx: number) => (
-				<SettingsButton key={label} label={label} route={buttonRoutes[indx]} />
+				<SettingsButton
+					key={label}
+					label={label}
+					route={buttonRoutes[indx]}
+					style={buttonStyle || styles.button}
+					textStyle={{ color: 'black' }}
+				/>
 			))}
 		</View>
 	);
@@ -36,5 +44,14 @@ const styles = StyleSheet.create({
 		fontSize: 20,
 		fontWeight: '500',
 		marginBottom: 17,
+	},
+	button: {
+		backgroundColor: 'white',
+		color: 'black',
+		padding: 10,
+		marginVertical: 5,
+		borderRadius: 8,
+		borderWidth: 1,
+		borderColor: 'black',
 	},
 });
--- texas-hearing-institute/pages/ListeningSettings/components/SettingsButton.tsx
+++ texas-hearing-institute/pages/ListeningSettings/components/SettingsButton.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import {
+	View,
+	TouchableOpacity,
+	Text,
+	StyleSheet,
+	ViewStyle,
+	TextStyle,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 interface SettingsButtonProps {
 	label: string;
 	route: string;
+	style?: ViewStyle;
+	textStyle?: TextStyle;
 }
 
 // Keep getting the error that the type to be passed into navigation.navigate must be type void, not sure why
@@ -13,16 +22,21 @@ interface Nav {
 	navigate: (value: string) => void;
 }
 
-export default function SettingsButton({ label, route }: SettingsButtonProps) {
+export default function SettingsButton({
+	label,
+	route,
+	style,
+	textStyle,
+}: SettingsButtonProps) {
 	const navigation = useNavigation<Nav>();
 
 	return (
 		<View>
 			<TouchableOpacity
-				style={styles.button}
+				style={[styles.button, style]}
 				onPress={() => navigation.navigate(route)}
 			>
-				<Text style={styles.buttonLabel}>{label}</Text>
+				<Text style={[styles.buttonLabel, textStyle]}>{label}</Text>
 			</TouchableOpacity>
 		</View>
 	);
@@ -35,6 +49,7 @@ const styles = StyleSheet.create({
 		height: 55,
 		marginTop: 6,
 		marginBottom: 6,
+		color: 'black',
 	},
 	selectedButton: {
 		backgroundColor: '#C0C0C0',
