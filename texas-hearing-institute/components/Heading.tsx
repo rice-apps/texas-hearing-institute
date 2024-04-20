6d4bc04ea8b68ad9aefd74e801376bd0fbbbc6da
Baylee Keevan <jade@baylees-laptop.dyndns.rice.edu>
2024-04-19 23:08:46 -0500

    report ui

--- texas-hearing-institute/components/Heading.tsx
+++ texas-hearing-institute/components/Heading.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Text } from 'react-native-elements';
-import tw from 'tailwind-react-native-classnames';
 
 interface HeaderProps {
 	title: string;
 }
 
 const Heading = ({ title }: HeaderProps) => {
-	return <Text style={tw`text-2xl font-bold py-5 px-4`}>{title}</Text>;
+	return (
+		<Text
+			style={{
+				color: '#333',
+				fontSize: 24,
+				fontWeight: 'bold',
+				marginTop: 32,
+				marginBottom: 28,
+				paddingHorizontal: 8,
+			}}
+		>
+			{title}
+		</Text>
+	);
 };
 
 export default Heading;
