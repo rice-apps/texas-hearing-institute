aebf416d800bd8e2b6cf4f8c0bae4d26951eea52
Shivam Pathak <58636209+pathaks1@users.noreply.github.com>
2024-06-13 21:08:41 -0700

    simple delete account page

---
+++ clinician-dashboard/app/delete_account/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+export default function DeleteAccount() {
+    return (
+        <div className="container mx-auto p-4">
+            <div className="card w-full bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <p className="text-lg text-white">To request your data be deleted from Speech Babble, email thi.riceapps@gmail.com with your child ID and associated first name and group ID for confirmation.</p>
+                    <p className="text-lg text-white">Afterwards, all report data, user data, and login data will be deleted and previous reports will not be accessible anymore.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
