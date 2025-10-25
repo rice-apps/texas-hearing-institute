50e87f8a8da0d3d8ca2fc5b4e201807fc0a8a1a3
Shivam Pathak <58636209+pathaks1@users.noreply.github.com>
2025-10-25 13:26:08 -0500

    reformat

--- clinician-dashboard/app/page.tsx
+++ clinician-dashboard/app/page.tsx
@@ -3,7 +3,7 @@ import LoginBox from './page_components/LoginBox';
 export default async function Home() {
   return (
   <div>
-    <h1>The clinician dashboard is no longer supported.</h1>
+    <p className="text-lg text-white">The clinician dashboard is no longer supported.</p>
     {/* <LoginBox /> */}
   </div>)
 }
