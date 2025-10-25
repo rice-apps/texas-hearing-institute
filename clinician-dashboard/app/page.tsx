505c1b53e36ae918d3797adcaa403b34b2e8533d
Shivam Pathak <58636209+pathaks1@users.noreply.github.com>
2025-10-25 13:21:39 -0500

    depracate dashboard

--- clinician-dashboard/app/page.tsx
+++ clinician-dashboard/app/page.tsx
@@ -3,6 +3,7 @@ import LoginBox from './page_components/LoginBox';
 export default async function Home() {
   return (
   <div>
-    <LoginBox />
+    <h1>The clinician dashboard is no longer supported.</h1>
+    {/* <LoginBox /> */}
   </div>)
 }
